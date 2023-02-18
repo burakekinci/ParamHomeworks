cf22e068daf1221bcbbbe2cef7e0eee0eb0e3751
burakekinci <burakekinci76@yandex.com>
2023-02-19 00:29:50 +0300

    CustomerController added

---
+++ ParamHW1/PracticumHW1/PracticumHW1/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticumHW1.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage = "Invalid Name Length!")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address!")]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage ="Invalid Phone Number!")]
+        public string PhoneNumber { get; set; }
+    }
+}
